f46be658a075c80848540b70699b97a1d3710a10
Will Boss <willboss@BossMac.local>
2026-04-06 15:39:27 +0100

    Remove old files

--- pages/_app.js
+++
@@ -1,7 +0,0 @@
-import '../styles/globals.css'
-import Layout from '../components/Layout'
-
-export default function App({ Component, pageProps }) {
-  if (Component.noLayout) return <Component {...pageProps} />
-  return <Layout><Component {...pageProps} /></Layout>
-}
